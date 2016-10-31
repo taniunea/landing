ddea7a2533a3871efae54fb1ad0dd2e049b07be0
Tatiana Nesterenco <tnesterenco@endava.com>
2016-10-31 18:06:55 +0200

    Responsive Galaxy S5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Gulp Starter kit")
+lightbox.option({showImageNumberLabel: false})
+
+$(".js-silky-smooth").on("click", "a", function() {
+    return $("html, body").animate({
+        scrollTop: $($.attr(this, "href")).offset().top
+    }, 500)
+});
+
+$('.map-container')
+	.dblclick(function(){
+			$(this).find('iframe').addClass('clicked')})
+	.mouseleave(function(){
+			$(this).find('iframe').removeClass('clicked')});
+
+
+/*$('.parallax-window').parallax({
+    naturalWidth: 1400,
+    naturalHeight: 900,
+    imageSrc: '/images/reviews.jpg'
+});*/
